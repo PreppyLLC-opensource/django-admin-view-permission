9c25ad20d03e66ca15ce63b5967cf74b54473929
Никита Конин <n.konin@onetrak.ru>
2017-02-08 10:51:31 +0300

    reduce code duplication

--- admin_view_permission/utils.py
+++ admin_view_permission/utils.py
@@ -12,3 +12,12 @@ def django_version():
         return DjangoVersion.DJANGO_18
     elif django.get_version().startswith('1.10'):
         return DjangoVersion.DJANGO_110
+
+
+def get_model_name(model):
+    if django_version() == DjangoVersion.DJANGO_18:
+        model_name = '%s.%s' % (model._meta.app_label,
+                                model._meta.object_name)
+    elif django_version() > DjangoVersion.DJANGO_18:
+        model_name = model._meta.label
+    return model_name
